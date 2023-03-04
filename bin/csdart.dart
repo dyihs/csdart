5674beffab54afaf7b921956664a142bceed6fed
shiyd <sydbgx@gmail.com>
2023-03-04 21:45:19 +0800

    创建dart项目

---
+++ bin/csdart.dart
@@ -0,0 +1,5 @@
+import 'package:csdart/csdart.dart' as csdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${csdart.calculate()}!');
+}
---
+++ test/csdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:csdart/csdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
