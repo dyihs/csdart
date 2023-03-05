49a5f91d7d69c1c402619adf98724372e74c55d4
shiyd <sydbgx@gmail.com>
2023-03-05 10:31:30 +0800

    一个简单的dart程序

---
+++ basic/chapter1/playground.dart
@@ -0,0 +1,12 @@
+// Define a function
+void printInteger(int aNumber) {
+  print('The number is $aNumber.');
+}
+
+// This is where the app starts executing.
+main() {
+  // Declare and initialize a variable.
+  var number = 42;
+  // Call a function.
+  printInteger(number);
+}
